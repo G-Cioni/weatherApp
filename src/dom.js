5123ab619f47aeadfe691b57595dce598106c2c5
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-10-04 02:51:03 +0200

    commented out unnecessary info-rendering functions

--- src/dom.js
+++ src/dom.js
@@ -1,49 +1,68 @@
 import { fetchCityData, getTemp, getHumidity, getCountry } from './fetch';
-import { convertToFahrenheit, convertToCelsius } from './miscellaneous';
+import {
+  convertToFahrenheit,
+  convertToCelsius,
+  convertToMph,
+  convertoToKph,
+  msToKph,
+  convertToKph,
+} from './converters';
 
-const name = document.getElementById('name');
+const name = document.getElementById('city');
 const temperature = document.getElementById('main-temperature');
 const humidity = document.getElementById('humidity');
-const high = document.getElementById('high');
-const low = document.getElementById('low');
 const country = document.getElementById('country');
-const weather = document.getElementById('weather-description');
+const weather = document.getElementById('weather');
 const windSpeed = document.getElementById('wind-speed');
-const windDirection = document.getElementById('wind-direction');
 const feelsLike = document.getElementById('feels-like');
 const celsiusBtn = document.getElementById('celsius');
 const fahrenheitBtn = document.getElementById('fahrenheit');
+// const high = document.getElementById('high');
+// const low = document.getElementById('low');
+// const windDirection = document.getElementById('wind-direction');
 
-function switchUnitPseudoElements(remove, add) {
-  low.classList.remove(remove);
-  low.classList.add(add);
-  high.classList.remove(remove);
-  high.classList.add(add);
+// function renderCityWindDirection(cityData) {
+//   windDirection.textContent = `${cityData.wind.deg}°`;
+// }
+
+function switchTempPseudoElements(remove, add) {
+  // low.classList.remove(remove);
+  // low.classList.add(add);
+  // high.classList.remove(remove);
+  // high.classList.add(add);
   feelsLike.classList.remove(remove);
   feelsLike.classList.add(add);
 }
 
-function convertAllTemp(convertFunc) {
-  temperature.textContent = convertFunc(temperature.textContent);
-  low.textContent = convertFunc(low.textContent);
-  high.textContent = convertFunc(high.textContent);
-  feelsLike.textContent = convertFunc(feelsLike.textContent);
+function switchSpeedPseudoElement(remove, add) {
+  windSpeed.classList.remove(remove);
+  windSpeed.classList.add(add);
 }
-function setFahrenheit() {
+
+function convertAll(convertTemp, convertSpeed) {
+  temperature.textContent = convertTemp(temperature.textContent);
+  // low.textContent = convertTemp(low.textContent);
+  // high.textContent = convertTemp(high.textContent);
+  feelsLike.textContent = convertTemp(feelsLike.textContent);
+  windSpeed.textContent = convertSpeed(windSpeed.textContent);
+}
+function setImperial() {
   if (celsiusBtn.classList.contains('active-unit')) {
     celsiusBtn.classList.remove('active-unit');
     fahrenheitBtn.classList.add('active-unit');
-    switchUnitPseudoElements('temperature-celsius', 'temperature-fahrenheit');
-    convertAllTemp(convertToFahrenheit);
+    switchTempPseudoElements('temp-metric', 'temp-imperial');
+    switchSpeedPseudoElement('speed-metric', 'speed-imperial');
+    convertAll(convertToFahrenheit, convertToMph);
   }
 }
 
-function setCelsius() {
+function setMetric() {
   if (fahrenheitBtn.classList.contains('active-unit')) {
     fahrenheitBtn.classList.remove('active-unit');
     celsiusBtn.classList.add('active-unit');
-    switchUnitPseudoElements('temperature-fahrenheit', 'temperature-celsius');
-    convertAllTemp(convertToCelsius);
+    switchTempPseudoElements('temp-imperial', 'temp-metric');
+    switchSpeedPseudoElement('speed-imperial', 'speed-metric');
+    convertAll(convertToCelsius, convertToKph);
   }
 }
 function capitalize(string) {
@@ -53,11 +72,7 @@ function capitalize(string) {
 }
 
 function renderCityWindSpeed(cityData) {
-  windSpeed.textContent = `${cityData.wind.speed} m/s`;
-}
-
-function renderCityWindDirection(cityData) {
-  windDirection.textContent = `${cityData.wind.deg}°`;
+  windSpeed.textContent = `${msToKph(cityData.wind.speed)}`;
 }
 
 function renderCityWeatherDesc(cityData) {
@@ -86,14 +101,14 @@ async function renderCityInfo(city) {
   const cityData = await fetchCityData(city);
   renderCityName(cityData);
   renderCityTemp(cityData, 'temp', 'C', temperature);
-  renderCityTemp(cityData, 'temp_max', 'C', high);
-  renderCityTemp(cityData, 'temp_min', 'C', low);
+  // renderCityTemp(cityData, 'temp_max', 'C', high);
+  // renderCityTemp(cityData, 'temp_min', 'C', low);
   renderCityTemp(cityData, 'feels_like', 'C', feelsLike);
   renderCityHumidity(cityData);
   renderCityCountry(cityData);
   renderCityWeatherDesc(cityData);
   renderCityWindSpeed(cityData);
-  renderCityWindDirection(cityData);
+  // renderCityWindDirection(cityData);
   return cityData;
 }
 
@@ -107,8 +122,8 @@ searchBtn.addEventListener('click', (e) => {
 renderCityInfo('roma,it');
 
 celsiusBtn.addEventListener('click', () => {
-  setCelsius();
+  setMetric();
 });
 fahrenheitBtn.addEventListener('click', () => {
-  setFahrenheit();
+  setImperial();
 });
