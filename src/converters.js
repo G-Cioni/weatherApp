8684ef8372ecd2812c3a04bdaaf4c0be71e3526f
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-10-09 03:11:34 +0200

    added time text converter

--- src/converters.js
+++ src/converters.js
@@ -17,6 +17,20 @@ function convertToKph(mphSpeed) {
   return Math.round(mphSpeed * 1.609 * 10) / 10;
 }
 
+function convertNumToTime(num) {
+  const stringifiedNum = num.toString();
+  return stringifiedNum.length === 1
+    ? `0${stringifiedNum}:00`
+    : `${stringifiedNum}:00`;
+}
+
+function addHours(time, addend) {
+  const hour = parseInt(time.slice(0, 2), 10) + 24;
+  let newTime = hour + addend;
+  newTime = newTime > 23 ? newTime - 24 : newTime;
+  return convertNumToTime(newTime);
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export {
   convertToFahrenheit,
@@ -24,4 +38,5 @@ export {
   convertToKph,
   convertToMph,
   msToKph,
+  addHours,
 };
